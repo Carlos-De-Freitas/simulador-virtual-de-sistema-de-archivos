5962d982bf89f0b7c22ee74f810ecb01c5f1ff3e
Daniel Farina <daniel.farina@correo.unimet.edu.ve>
2025-11-25 19:08:04 -0400

    Modos de usuario y lógica de usuario

---
+++ src/modos/User.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package security;
+
+/**
+ *
+ * @author 58412
+ */
+public class User {
+
+    private final String username;
+    private final UserMode mode;
+
+    public User(String username, UserMode mode) {
+        this.username = username;
+        this.mode = mode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserMode getMode() {
+        return mode;
+    }
+
+    public boolean isAdmin() {
+        return mode == UserMode.ADMIN;
+    }
+}
---
+++ src/modos/UserMode.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package security;
+
+/**
+ *
+ * @author 58412
+ */
+public enum UserMode {
+    ADMIN,
+    USER
+}
